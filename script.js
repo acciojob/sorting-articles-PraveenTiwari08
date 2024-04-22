4e9e973a1cbc3e77ca6ea9735c390acaa31341fe
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-23 01:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let list = document.getElementById("band");
+const bands = ['The Plot in You', 
+			   'The Devil Wears Prada', 
+			   'Pierce the Veil', 
+			   'Norma Jean', 
+			   'The Bled', 'Say Anything', 'The Midway State', 
+			   'We Came as Romans', 'Counterparts', 
+			   'Oh, Sleeper', 'A Skylit Drive', 
+			   'Anywhere But Here', 'An Old Dog'];
+
+
+let listHTML = "<ul>";
+listItems.forEach(item => {
+  listHTML += `<li>${item}</li>`;
+});
+listHTML += "</ul>";
+
+list.innerHTML = listHTML;
